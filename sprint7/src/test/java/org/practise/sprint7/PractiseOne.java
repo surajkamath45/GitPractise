59b72005cab086420a5ce3c8735dbe3509b3a241
Suraj <Suraj@R2D2>
2018-04-26 20:51:51 +0530

    Test

---
+++ sprint7/src/test/java/org/practise/sprint7/PractiseOne.java
@@ -0,0 +1,40 @@
+package org.practise.sprint7;
+
+import java.awt.Robot;
+import java.io.IOException;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.interactions.Actions;
+import org.testng.annotations.Test;
+
+public class PractiseOne {
+
+	WebDriver driver;
+	Robot robot;
+	Actions acts;
+
+	public PractiseOne() {
+		System.setProperty("webdriver.gecko.driver", "F:\\My_Workspace\\drivers\\geckodriver.exe");
+		driver = new FirefoxDriver();
+		driver.manage().window().maximize();
+		driver.findElement(By.tagName("html")).sendKeys(Keys.chord(Keys.CONTROL, "0"));
+		driver.get("http://demo.guru99.com/test/image_upload/index.php");
+
+	}
+
+	@Test
+	public void testOne() {
+
+		WebElement chooseFileButton = driver.findElement(By.cssSelector("input#photoimg"));
+		chooseFileButton.sendKeys("F:\\OpenButton.png");
+		try {
+			Runtime.getRuntime().exec("F:\\My_Workspace\\AutoIT\\testAutoIT\\testOne.exe");
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+}
